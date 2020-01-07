0352157d083248dd2ee92accff6a8d3f4e7d8214
Tenzin Sean <tenzin.jeunesse@gmail.com>
2020-01-07 16:36:45 +0100

    customized admin page and add some model

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    
+    """ Item admin Defination """
+
+    pass
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """ Room admin Defination """
+
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
+        ), 
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")}
+        ),
+         (
+            "Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "More About the Space",
+            
+            {"fields": ("amenities", "facilites", "house_rules"), 'classes': ('collapse'),}
+        ),
+        (
+            "Last Details", 
+            {"fields": ("host",)}
+        )
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "city",
+        "room_type",
+        "amenities",
+        "facilites",
+        "house_rules",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilites",
+        "house_rules"
+    )
+
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "hello"
+    
+    count_amenities.short_description = "Hello there"
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Photo model Defination """
+
+    pass
+
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType abstract """
+
+    class Meta:
+        verbose_name_plural = "Room Type"
+        ordering = ['created']
+
+
+class Amenity(AbstractItem):
+    """ Amenity object defination """
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model defination """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """ House rule model defination """
+
+    class Meta:
+        verbose_name_plural = "House Rules"
+
+
+class Photo(core_models.TimeStampModel):
+
+    """ Photo of model Defination """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.caption
+
 
 # Create your models here.
+
+class Room(core_models.TimeStampModel):
+
+    """ Room models Definiation """
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=40)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilites = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
