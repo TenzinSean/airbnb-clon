a7d23a4dff35339bb0adbf3beb5261b6f2c02ca9
Tenzin Sean <tenzin.jeunesse@gmail.com>
2020-01-16 21:09:42 +0100

    added style login and sign up

--- users/views.py
+++ users/views.py
@@ -30,11 +30,7 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class =  forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        'first_name': "Tenzin",
-        'last_name': "Sean",
-        'email': 'tenzin@gmail.com',
-    }
+    
 
 
     def form_valid(self, form):
