0beebcd8611d184c9a77bab811c08ccbd0490627
Tenzin Sean <tenzin.jeunesse@gmail.com>
2020-01-17 16:03:02 +0100

    added style room details

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+from django.shortcuts import reverse
 
 # Create your models here.
 
@@ -54,6 +55,8 @@ class User(AbstractUser):
     email_secret = models.CharField(max_length=20, default="", blank=True)
     login_method = models.CharField(max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
 
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={'pk': self.pk})
 
     def verify_email(self):
         if self.email_confirmed is False:
--- users/views.py
+++ users/views.py
@@ -1,13 +1,18 @@
 import os
 import requests
+from django.contrib.auth.views import PasswordChangeView
 from django.views import View
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms, models
+from . import forms, models, mixins
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
-class LoginView(FormView):
+
+
+class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class =  forms.LoginForm
@@ -20,13 +25,21 @@ class LoginView(FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 def log_out(request):
+    messages.info(request, "See you later")
     logout(request)
     return redirect(reverse("core:home"))
 
 
-class SignUpView(FormView):
+class SignUpView(mixins.LoggedOutOnlyView, FormView):
     template_name = "users/signup.html"
     form_class =  forms.SignUpForm
     success_url = reverse_lazy("core:home")
@@ -72,16 +85,16 @@ def github_callback(request):
         client_secret = os.environ.get("GH_SECRET")
         code = request.GET.get("code", None)
         if code is not None:
-            result = requests.post(
+            token_request = requests.post(
                 f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
                 headers={"Accept": "application/json"},
             )
-            result_json = result.json()
-            error = result_json.get("error", None)
+            token_json = token_request.json()
+            error = token_json.get("error", None)
             if error is not None:
-                raise GitHubException()
+                raise GitHubException("Can't access token")
             else:
-                access_token = result_json.get("access_token")
+                access_token = token_json.get("access_token")
                 profile_request = requests.get(f"https://api.github.com/user", headers={
                     "Authorization": f"token {access_token}",
                     "Accept": "application/json", 
@@ -96,26 +109,87 @@ def github_callback(request):
                     try:
                         user = models.User.objects.get(email=email)
                         if user.login_method != models.User.LOGIN_GITHUB:
-                            raise GitHubException()
+                            raise GitHubException(f"Please log in with {user.login_method}")
                     except models.User.DoesNotExist:
                         user = models.User.objects.create(
                             email=email, 
                             first_name=name, 
                             username=email,
                             bio=bio, 
-                            login_method=models.User.LOGIN_GITHUB
+                            login_method=models.User.LOGIN_GITHUB,
+                            email_confirmed=True,
                         )
                         user.set_unusable_password()
                         user.save()
-                        login(request, user)
+                    login(request, user)
+                    messages.success(request, f"Welcome back {user.first_name}")
                     return redirect(reverse("core:home"))
                 else:
-                    raise GitHubException()
+                    raise GitHubException("Can't get your profile")
         else:
-            raise GitHubException()
-    except GitHubException:
+            raise GitHubException("Can't get code dick")
+    except GitHubException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
+
+class UserProfileView(DetailView):
+    model = models.User
+    context_object_name = "user_obj"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["hello"] = "Hello"
+        return context
+
+class UpdateProfileView(mixins.LoggedInOnlyView, UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "langauge",
+        "currency",
+    )
+    
+    success_message = "Profile Updated"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["first_name"].widget.attrs = {"placeholder": "First name"}
+        form.fields["last_name"].widget.attrs = {"placeholder": "Last name"}
+        form.fields["bio"].widget.attrs = {"placeholder": "Bio"}
+        form.fields["birthdate"].widget.attrs = {"placeholder": "Birthdate"}
+        form.fields["first_name"].widget.attrs = {"placeholder": "First name"}
+        return form
+    
+
+class UpdatePasswordView(
+    mixins.EmailLoginOnlyView, 
+    mixins.LoggedOutOnlyView, 
+    SuccessMessageMixin, 
+    PasswordChangeView):
+    template_name = "users/update-password.html"
+    success_message = "Password Updated"
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["old_password"].widget.attrs = {"placeholder": "Current password"}
+        form.fields["new_password1"].widget.attrs = {"placeholder": "New password"}
+        form.fields["new_password2"].widget.attrs = {
+            "placeholder": "Confirm new password"
+        }
+        return form
+    def get_success_url(self):
+        return self.request.user.get_absolute_url()
 
 
 
